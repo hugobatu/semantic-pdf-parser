abdfd45f33f8718f621075bfacead244564f328c
Minh Quan <quanhugo2505@gmail.com>
2025-10-28 23:17:16 +0700

    Add langchain configration + pipeline for model

--- src/Mapper.py
+++ src/Mapper.py
@@ -7,7 +7,17 @@ def mapping_excel(map_file: str, target_file: str):
     merged_df = df2.merge(df1, left_on="Course ID", right_on="course_id", how="left")
     merged_df.rename(columns={"course_name": "Vietnamese Course Name"}, inplace=True)
     merged_df.drop('course_id', axis=1, inplace=True)
-    merged_df.to_excel("./Excel/course_reading_lists_with_vi.xlsx", index=False)
+    if "Year" in merged_df.columns:
+        merged_df["Year"] = (
+            merged_df["Year"]
+            .astype(str)
+            .str.replace("'", "", regex=False)
+            .str.strip()
+            .replace("", pd.NA)  
+        )
+        merged_df["Year"] = pd.to_numeric(merged_df["Year"], errors="coerce").astype("Int64")
+
+    merged_df.to_excel("../output/course_reading_lists_with_vi.xlsx", index=False)
 
     print(f"Merged successfully: {len(merged_df)} rows")
     print(merged_df.head())
--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
-from TableOfContentExtractor import extract_course_reading_lists
-from ExcelExporter import export_to_excel
+# from TableOfContentExtractor import extract_course_reading_lists
+# from ExcelExporter import export_to_excel
 from Mapper import mapping_excel
 
 if __name__ == "__main__":
     
     pdf_file = "./input/Module_Handbook_Physics.pdf" 
-    mapping_file = "./mapper_excel/Course-Vi-unique.xlsx"
-    default_output = "./output/course_reading_lists_with_vietnamese_name.xlsx"
+    mapping_file = "../mapper_excel/Course-Vi-unique.xlsx"
+    target_file = "../output/course_reading_lists.xlsx"
     
     # extract raw reading list text from PDF
-    all_reading_lists = extract_course_reading_lists(pdf_file)
+    # all_reading_lists = extract_course_reading_lists(pdf_file)
     
     # llm context recongnition and exporting to Excel
-    export_to_excel(all_reading_lists, "output")
+    # export_to_excel(all_reading_lists)
 
     # mapping vietnamese name
-    mapping_excel(mapping_excel, default_output)
+    mapping_excel(mapping_file, target_file)
 
 
     # for course in all_reading_lists:
